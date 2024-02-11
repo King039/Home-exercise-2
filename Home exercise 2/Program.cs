16dd951f0e0e2eb73c5a4db73b876f3d999734cd
King039 <bazanks039@gmail.com>
2024-02-11 19:52:35 +0200

    Добавьте файлы проекта.

---
+++ Home exercise 2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+// дз 10.1
+
+//Console.Write("Введите радиус:");
+//double R = double.Parse(Console.ReadLine());
+//double S = Math.PI * R * R;
+//double L = 2 * Math.PI * R;
+//Console.WriteLine($"S={S:F2}");
+//Console.WriteLine($"P={L:F2}");
+
+// дз 10.2
+
+//Console.Write("Введите катет А: ");
+//double A = double.Parse(Console.ReadLine());
+//Console.Write("Введите гипотенузу C: ");
+//double C = double.Parse(Console.ReadLine());
+//double B = Math.Sqrt(C * C - A * A);
+//Console.WriteLine($"{B:F2}");
