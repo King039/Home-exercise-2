b61ea6c2d2b54c4d59ef81d1a9616a39311a4906
King039 <bazanks039@gmail.com>
2024-02-18 10:44:31 +0200

    6.1

--- Home exercise 2/Program.cs
+++ Home exercise 2/Program.cs
@@ -1,4 +1,80 @@
-﻿////9.2
+﻿//дз 5.1
+//const double MM_TO_CM = 0.1;
+//const double MM_TO_M = 0.0001;
+//const double MM_TO_KM = 0.000001;
+//const double MM_TO_MILI = 6.213711922373341e-7;
+//const double MM_TO_YD = 0.0010936132983;
+
+
+//Console.WriteLine("Введите колличество мм: ");
+//double mm = double.Parse(Console.ReadLine());
+//double cm = mm * MM_TO_CM;
+//Console.WriteLine($"{cm}см");
+
+
+//double m = mm * MM_TO_M;
+//Console.WriteLine($"{m}м");
+
+
+//double km = mm * MM_TO_KM;
+//Console.WriteLine($"{km}км");
+
+
+//double mili = mm * MM_TO_MILI;
+//Console.WriteLine($"{mili}мили");
+
+
+//double yd = mm * MM_TO_YD;
+//Console.WriteLine($"{yd}ярд");
+
+
+// дз 5.2
+// Первый продукт "кофе стандарт" цена 69,0 ; колличество 1
+
+//Console.WriteLine("Укажите продукт: ");
+//string firstProductName = Console.ReadLine();
+//Console.WriteLine($"Укажите цену {firstProductName}: ");
+//decimal firstProductPrice = decimal.Parse(Console.ReadLine());
+//Console.WriteLine($"Укажите колличество продукта {firstProductName} , по цене {firstProductPrice}: ");
+//int firstProductQuantity = int.Parse(Console.ReadLine());
+//decimal sum = firstProductPrice * firstProductQuantity;
+
+////// Второй продукт "рулет-спринг тв из ку" цена 70,0 ; колличество 1
+
+//Console.WriteLine("Укажите продукт: ");
+//string secondProductName = Console.ReadLine();
+//Console.WriteLine($"Укажите цену {secondProductName}: ");
+//decimal secondProductPrice = decimal.Parse(Console.ReadLine());
+//Console.WriteLine($"Укажите колличество продукта {secondProductName} , по цене {secondProductPrice}: ");
+//int secondProductQuantity = int.Parse(Console.ReadLine());
+//decimal sum2 = secondProductPrice * secondProductQuantity;
+
+//Console.WriteLine("*********************************************************");
+//Console.WriteLine("№ Название\t\t\tЦена\tКолличество\tСумма");
+//Console.WriteLine($"№1 {firstProductName}\t\t{firstProductPrice}\t{firstProductQuantity}\t\t{sum}");
+//Console.WriteLine("НДС с расчитаной ставкой 18%\t\t\t\t10.53");
+//Console.WriteLine($"№2 {secondProductName}\t{secondProductPrice}\t{secondProductQuantity}\t\t{sum2}");
+//Console.WriteLine("НДС с расчитаной ставкой 10%\t\t\t\t6.36");
+//Console.WriteLine("*********************************************************");
+//Console.WriteLine($"Итого: {sum + sum2}");
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+//Console.WriteLine($"Электронные средства:\t\t\t\t{sum + sum2}");
+//Console.WriteLine("НДС итога чека с расчитаной ставкой\n18%\t10.53");
+//Console.WriteLine("НДС итога чека с расчитаной ставкой\n10%\t6.36");
+//Console.WriteLine(" ");
+//Console.WriteLine(" ");
+//Console.WriteLine("*********************************************************");
+//Console.WriteLine("ВИД НАЛОГООБЛАЖЕНИЯ: ОСН");
+//Console.WriteLine("РЕГ. НОМЕР ККТ: 0000143873023483");
+//Console.WriteLine("ЗАВОД. №:");
+//Console.WriteLine("ФН. №: 8710000100265168");
+//Console.WriteLine("ФД. №: 6026");
+//Console.WriteLine("ФПД: 3322278314");
+
+
+
+////9.2
 ////1
 //long a = 264895468854;
 //Console.WriteLine(a + "=" + Convert.ToString(a, 2));
@@ -142,32 +218,32 @@
 
 //11.3
 //1
-Random random = new Random();
-Console.WriteLine("Введите и угадайте число от 0 до 5 загаданое компьютером:");
-int userNumber = int.Parse(Console.ReadLine());
-int number = random.Next(0, 6);
-Console.WriteLine(number);
-if (userNumber == number) Console.WriteLine("Победа!");
-else Console.WriteLine("Вы не угадали");
-//2
-Console.Write("Введите номер месяца:");
-byte n = byte.Parse(Console.ReadLine());
-Console.Write("Введите температуру:");
-double t = double.Parse(Console.ReadLine());
-switch (n)
-{
-    case 1:
-    case 2:
-    case 12:
-        {
-            if (t > 0) Console.WriteLine("Дождьливая зима");
-            Console.WriteLine("Зима"); break;
-        }
-    case 3: case 4: case 5: Console.WriteLine("Весна"); break;
-    case 6: case 7: case 8: Console.WriteLine("Лето"); break;
-    case 9: case 10: case 11: Console.WriteLine("Осень"); break;
-    default: Console.WriteLine("Такого месяца нет"); break;
-}
+//Random random = new Random();
+//Console.WriteLine("Введите и угадайте число от 0 до 5 загаданое компьютером:");
+//int userNumber = int.Parse(Console.ReadLine());
+//int number = random.Next(0, 6);
+//Console.WriteLine(number);
+//if (userNumber == number) Console.WriteLine("Победа!");
+//else Console.WriteLine("Вы не угадали");
+////2
+//Console.Write("Введите номер месяца:");
+//byte n = byte.Parse(Console.ReadLine());
+//Console.Write("Введите температуру:");
+//double t = double.Parse(Console.ReadLine());
+//switch (n)
+//{
+//    case 1:
+//    case 2:
+//    case 12:
+//        {
+//            if (t > 0) Console.WriteLine("Дождьливая зима");
+//            Console.WriteLine("Зима"); break;
+//        }
+//    case 3: case 4: case 5: Console.WriteLine("Весна"); break;
+//    case 6: case 7: case 8: Console.WriteLine("Лето"); break;
+//    case 9: case 10: case 11: Console.WriteLine("Осень"); break;
+//    default: Console.WriteLine("Такого месяца нет"); break;
+//}
 
 //11.4
 
@@ -187,3 +263,4 @@ switch (n)
 //    case 4: Console.WriteLine("Дядя Фёдор - Чтобы продать что-нибудь не нужное, надо купить что-нибудь не нужное, а унас денег нет!"); break;
 //    default: Console.WriteLine("Такого кандидата нет!"); break;
 //}
+
